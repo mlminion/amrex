779f4a23e0fe9df0f1890bd7034cfb1f02197bfb
Kerrek Stinson <kerrekstinson@Kerreks-MacBook-Pro.local>
2020-06-25 19:58:42 -0400

    WIP: added tolerance and relaxation+smoother parameters (see bottom of README)

--- Src/LinearSolvers/MLMG/AMReX_MLABecLaplacian.cpp
+++ Src/LinearSolvers/MLMG/AMReX_MLABecLaplacian.cpp
@@ -1,7 +1,7 @@
 
 #include <AMReX_MLABecLaplacian.H>
 #include <AMReX_MultiFabUtil.H>
-
+#include <AMReX_ParmParse.H>
 #include <AMReX_MLABecLap_K.H>
 
 namespace amrex {
@@ -29,6 +29,13 @@ MLABecLaplacian::define (const Vector<Geometry>& a_geom,
     MLCellABecLap::define(a_geom, a_grids, a_dmap, a_info, a_factory);
 
     const int ncomp = getNComp();
+    
+    ParmParse pp;
+    m_relaxation_parameter = 1.0;
+    pp.query("relaxation_parameter",m_relaxation_parameter);
+
+    m_jacobi_smooth = true;
+    pp.query("jacobi_smooth",m_jacobi_smooth);
 
     m_a_coeffs.resize(m_num_amr_levels);
     m_b_coeffs.resize(m_num_amr_levels);
@@ -392,11 +399,22 @@ MLABecLaplacian::Fsmooth (int amrlev, int mglev, MultiFab& sol, const MultiFab& 
             }
             else if ((opOrder() == 244) && (AMREX_SPACEDIM == 2))
             {
-                // abec_x_high, where x = jacobi or gsrb. Jacobi (currently) needs relaxation in periodic case.
-                abec_gsrb_high(thread_box, solnfab, rhsfab, alpha, dhx, dhy,
+                
+                if(m_jacobi_smooth)
+                {
+                    // abec_x_high, where x = jacobi or gsrb. Jacobi (currently) needs relaxation in periodic case.
+                    abec_jacobi_high(thread_box, solnfab, rhsfab, alpha, dhx, dhy,
                                afab, bxfab, byfab,
                                m0, m1, m2, m3,
-                               vbx, nc, phi_tmpfab, 1.0); // last input is relaxation param
+                               vbx, nc, phi_tmpfab, m_relaxation_parameter); // last input is relaxation param
+                }
+                else
+                {
+                    abec_gsrb_high(thread_box, solnfab, rhsfab, alpha, dhx, dhy,
+                                     afab, bxfab, byfab,
+                                     m0, m1, m2, m3,
+                                     vbx, nc, phi_tmpfab, m_relaxation_parameter);
+                }
                 
             }
             else
